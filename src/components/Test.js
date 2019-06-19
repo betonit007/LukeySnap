6c76eac7317fa3e1127a7a410fb8a4be6d37aa93
betonit007 <timnagorski@hotmail.com>
2019-06-19 14:35:08 -0400

    basic functionality

--- src/components/Test.js
+++ src/components/Test.js
@@ -5,25 +5,73 @@ import firebase from './firebase';
 class Test extends React.Component{
    
     state = {
-        speed: 10
+        snapping: '',
+        snaps: '',
+    }
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value,
+        })
+
+        console.log(this.state.snaps);
     }
 
     componentDidMount() {
-        var database = firebase.database();
+        let database = firebase.database();
         
-        database.ref().on("value", snap => {
+        database.ref().on("value", snapshot => {
+            const snapObj = snapshot.val();
+            Object.keys(snapObj).map(i => console.log(snapObj[i].snap)) //using Object.keys to map thru obj like array
+            this.setState({ snaps: snapObj });
             
-            this.setState({ speed: snap.val().age });
            
         })
     };
+
+    handleSubmit() {
+        let database = firebase.database();
+        database.ref().push({
+            snap: this.state.snapping
+        })
+        console.log(this.state.snap);
+        this.setState({ snapping: ''})
+    }
+
+    renderSnaps() {
+        if (!this.state.snaps) {
+            return null
+        }
+        else {
+            return (
+                Object.keys(this.state.snaps).map(
+                    snap => (this.state.snaps[snap].snap 
+                        ? <div>{this.state.snaps[snap].snap}</div> 
+                        : null))
+            )
+        }
+    }
     
 
     render() {
         return (
-            <div>
-                {this.state.speed}
-            </div>
+              <div className='container'>
+
+                <div className='jumbotron'>
+                    {this.renderSnaps()}
+                </div>
+              
+                <div className="input-group input-group-lg">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="inputGroup-sizing-lg">Text</span>
+                    </div>
+                    <input onChange={this.handleInputChange} type="text" className="form-control" name="snapping" value={this.state.snapping}/>
+                </div>
+                <button className="btn-primary btn" style={{marginTop:"5px"}} onClick={()=>this.handleSubmit()}>Submit</button>
+
+              </div>
 
         )
     }
