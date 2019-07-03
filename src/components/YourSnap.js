72bc90dd10dde1693bced36ad4b696aeb2f3b9d0
betonit007 <timnagorski@hotmail.com>
2019-07-03 10:28:54 -0400

    justify content (one renderSnap)

--- src/components/YourSnap.js
+++ src/components/YourSnap.js
@@ -11,7 +11,7 @@ const snapStyle = {
 
 const YourSnap = (props) => {
     return (
-        <div className='d-flex'>
+      <div className={!props.id ? 'd-flex' : props.id.UserId === props.fireId ? 'd-flex justify-content-end' : ''}>
           <div className="alert alert-primary" role="alert" style={{maxWidth:'75%'}}>
             <span><img className='img-fluid img-thumbnail'style={{height:'45px', width:'45px'}} alt={props.name} src={props.img}></img></span>
              <span style={someStyle}>{props.name}- </span><span style={snapStyle}>{props.snapText}</span>
